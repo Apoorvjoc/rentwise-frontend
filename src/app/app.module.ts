8a4605c798cf2f7e620d7dee48936a457448776d
Apoorvjoc <hlwapoorv@gmail.com>
2024-12-23 20:18:34 +0530

    setup bootstrap

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { NgModule } from "@angular/core";
+
+@NgModule({
+    declarations: [
+      
+    ],
+    imports: [
+      BrowserModule
+    ],
+    providers: []
+ })
+  export class AppModule { }
+  
